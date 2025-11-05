4cb91351ba93d0e533a33d47539b531aa0aeb3aa
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-04 23:45:18 -0300

    feat: create OTP validator

--- src/services/otp.ts
+++ src/services/otp.ts
@@ -27,3 +27,37 @@ export const generateOTP = async (userId: number) => {
 
     return otp
 }
+
+export const validateOTP = async (id: string, code: string) => {
+
+    const otp = await prisma.otp.findFirst({
+
+        select: {
+            user: true
+        },
+
+        where: {
+            id,
+            code,
+            expiresAt: {
+                gt: new Date()
+            },
+
+            used: false
+        }
+    })
+
+    //  Validate OTP
+    if (otp && otp.user) {
+
+        await prisma.otp.update({
+
+            where: { id },
+            data: { used: true }
+        })
+
+        return otp.user
+    }
+
+    return false;
+}
