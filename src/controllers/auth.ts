3f234c8d9797088bd9cdb8e1660d512ab3756a1a
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-04 23:50:34 -0300

    feat: validates in imports, useOTP, signup

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from "express";
 import { authSigninSchema } from "../schemas/auth-signin";
-import { getUserByEmail } from "../services/user";
-import { generateOTP } from "../services/otp";
+import { authSignUpSchema } from "../schemas/auth-signup";
+import { createUser, getUserByEmail } from "../services/user";
+import { generateOTP, validateOTP } from "../services/otp";
+import { sendEmail } from "../libs/mailtrap";
+import { authUserOTPSchema } from "../schemas/auth-userotp";
 
 export const signin: RequestHandler = async (req, res) => {
 
@@ -24,7 +27,61 @@ export const signin: RequestHandler = async (req, res) => {
     const otp = await generateOTP(user.id)
 
     // Send email to user
+    await sendEmail(
+
+        user.email,
+        'Your access code is: ' + otp.code,
+        'Enter your code: ' + otp.code
+    )
 
     // Return OTP ID CODE
     res.json({ id: otp.id })
 };
+
+export const signup: RequestHandler = async (req, res) => {
+
+    // Validate received data 
+    const data = authSignUpSchema.safeParse(req.body);
+
+    if (!data.success) {
+        res.json({ error: data.error.flatten().fieldErrors });
+        return;
+    }
+
+    // Validate if the email exists 
+    const user = await getUserByEmail(data.data.email)
+
+    if (user) {
+        res.json({ error: 'User with    this email already' })
+        return
+    }
+
+    // Create User
+    const newUser = await createUser(data.data.name, data.data.email)
+
+    // Return data of the newly created user
+    res.status(201).json({ user: newUser })
+}
+
+export const useOTP: RequestHandler = async (req, res) => {
+
+    // Validate received data
+    const data = authUserOTPSchema.safeParse(req.body);
+
+    if (!data.success) {
+        res.json({ error: data.error.flatten().fieldErrors });
+        return;
+    }
+
+    // Validate OTP
+    const user = await validateOTP(data.data.id, data.data.code)
+
+    if (!user) {
+        res.json({ error: 'Invalid or expired OTP' })
+        return
+    }
+
+    // Create JWT
+
+    // Return JWT
+}
