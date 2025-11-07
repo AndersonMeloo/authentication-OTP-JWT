ddde9a2ba8dcc158fbb1549f214a012d564fb997
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-11-06 23:08:47 -0300

    feat: zod userotp

--- src/schemas/auth-userotp.ts
+++ src/schemas/auth-userotp.ts
@@ -3,5 +3,5 @@ import z from "zod";
 export const authUserOTPSchema = z.object({
 
     id: z.string({ message: 'Id OTP Required' }),
-    code: z.string().length(6, 'Code must have 6 digits')
+    code: z.string({ message: 'OTP Required' }).length(6, 'Code must have 6 digits')
 })
